6a88c0059fef339823eaca1b8dba7e9a1213d83d
damibless97 <empgentle@gmail.com>
2025-06-09 22:42:15 +0100

    view52

--- list/serializers.py
+++ list/serializers.py
@@ -6,8 +6,5 @@ class WaitlistUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = WaitlistUser
-        fields = ['email', 'referral_code', 'referrals_count']
-
-    def get_referrals_count(self, obj):
-        return WaitlistUser.objects.filter(referred_by=obj.referral_code).count()
+        fields = ['email']
 
