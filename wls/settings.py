6c9ed9f34a8a92a541e0583626fa46970a6ff671
damibless97 <empgentle@gmail.com>
2025-06-09 22:02:20 +0100

    cors

--- wls/settings.py
+++ wls/settings.py
@@ -46,9 +46,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'list',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,6 +58,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://odanforext.xyz",
+    "https://wlsdd.onrender.com/api/join/",
 ]
 
 ROOT_URLCONF = 'wls.urls'
