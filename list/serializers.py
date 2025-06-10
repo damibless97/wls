fb1b2e541fd679c5652f3759924177491c0bae05
damibless97 <empgentle@gmail.com>
2025-06-10 09:06:18 +0100

    serializer

--- list/serializers.py
+++ list/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import WaitlistUser
 
 class WaitlistUserSerializer(serializers.ModelSerializer):
-    referrals_count = serializers.SerializerMethodField()
 
     class Meta:
         model = WaitlistUser
